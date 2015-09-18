0f1dd295a62e3368cb54acaa33d9919f2b99c402
Brunel Odegard <bcodegard@gmail.com>
2015-09-17 17:31:06 -0700

    Added docstrings to ACBOX

--- ad5764_acbox.py
+++ ad5764_acbox.py
@@ -66,20 +66,21 @@ class AD5764AcboxWrapper(DeviceWrapper):
         yield p.send()
         
     def packet(self):
-        """Create a packet in our private context."""
+        """Create a packet in our private context"""
         return self.server.packet(context=self.ctx)
 
     def shutdown(self):
-        """Disconnect from the serial port when we shut down."""
+        """Disconnect from the serial port when we shut down"""
         return self.packet().close().send()
 
     @inlineCallbacks
     def write(self, code):
-        """Write a data value to the heat switch."""
+        """Write a data value to the ACBOX"""
         yield self.packet().write(code).send()
 
     @inlineCallbacks
     def read(self):
+        """Read a response line from the ACBOX"""
         p=self.packet()
         p.read_line()
         ans=yield p.send()
@@ -96,9 +97,10 @@ class AD5764AcboxWrapper(DeviceWrapper):
 
 
 class AD5764AcboxServer(DeviceServer):
-    name          = 'ad5764_acbox'
-    deviceName    = 'Arduino Acbox'
-    deviceWrapper = AD5764AcboxWrapper
+    name             = 'ad5764_acbox'
+    deviceName       = 'Arduino Acbox'
+    deviceWrapper    = AD5764AcboxWrapper
+    clock_multiplier = 5
 
     @inlineCallbacks
     def initServer(self):
@@ -139,6 +141,11 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(201,clock_multiplier='i',returns='s' )
     def initialize(self,c,clock_multiplier=5):
+        """Initializes the device. Usage: initialize(clock_multiplier)"""
+        yield
+        self.clock_multiplier = clock_multiplier
+        if (clock_multiplier < 4) or (clock_multiplier > 20):
+            returnValue("Error: clock multiplier must be bewteen 4 and 20.")
         dev=self.selectedDevice(c)
         yield dev.write("NOP\r") # clear input buffer
         yield dev.read()         # clear output buffer
@@ -148,6 +155,13 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(202,channel='s',value='v',returns='s')
     def set_channel_voltage(self,c,channel,value):
+        """Sets the voltage for a channel. Usage is set_channel_voltage(channel, fraction_of_full_scale)
+Channel must be "X1" "Y1" "X2" or "Y2" and the value must be from 0 to 1"""
+        yield
+        if not (0<=value<=1):
+            returnValue("Error: magnitude value must be from 0 to 1 (inclusive.)")
+        if not (channel in ['X1','X2','Y1','Y2']):
+            returnValue("Error: channel must be either \"X1\" \"Y1\" \"X2\" or \"Y2\"")
         dev=self.selectedDevice(c)
         yield dev.write("SET,%s,%f\r"%(channel,value))
         ans = yield dev.read()
@@ -161,6 +175,9 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(203,offset='v',returns='s')
     def set_phase(self,c,offset):
+        """Sets the phase offset between the boards (1 and 2.) Usage is set_phase(offset_in_degrees)
+The offset is taken modulo 360."""
+        offset = offset%360.0
         dev=self.selectedDevice(c)
         yield dev.write("PHS,1,%f\r"%offset)
         yield dev.write("PHS,2,0\r")
@@ -176,12 +193,17 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(204,frequency='v',returns='s')
     def set_frequency(self,c,frequency):
+        """Sets the frequency of the two boards. Usage is set_frequency(frequency_in_Hz)
+Frequency must be at least 1, and full scale, which is 20MHz * clock_multiplier
+where clock_multiplier is the multiplier set with the initialize function."""
         dev=self.selectedDevice(c)
-        
+
+        yield
         if frequency <= 0:
-            yield None
             returnValue("Error: frequency cannot be zero (or less.)")
-            
+        if frequency > (self.clock_multiplier * 20000000):
+            returnValue("Error: frequency cannot exceed 20MHz * clock_multiplier, where clock_multiplier is the multiplier set with the initialize function. (currently %i)"%self.clock_multiplier)            
+
         yield dev.write("FRQ,%f\r"%frequency)
         ans = yield dev.read()
         
@@ -194,6 +216,7 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(205,returns='s')
     def identify(self,c):
+        """Queries the *IDN? command and returns the response. Usage is identify()"""
         dev=self.selectedDevice(c)
         yield dev.write("*IDN?\r")
         ans = yield dev.read()
@@ -201,6 +224,7 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(206,returns='s')
     def get_is_ready(self,c):
+        """Queries the *RDY? command and returns the response. Usage is get_is_ready()"""
         dev=self.selectedDevice(c)
         yield dev.write("*RDY?\r")
         ans = yield dev.read()
@@ -208,6 +232,8 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(207,returns='s')
     def reset(self,c):
+        """Performs a master reset. Usage is reset()
+Once this is called, initialize must be called again before the device outputs a signal."""
         dev=self.selectedDevice(c)
         yield dev.write("MR\r")
         ans = yield dev.read()
@@ -215,6 +241,9 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(208,returns='s')
     def update_boards(self,c):
+        """Signals the boards to update. Usage is update_boards()
+This shouldn't be necessary, (the device should do this automatically) but
+if the boards aren't responding to changes, try calling this function."""
         dev=self.selectedDevice(c)
         yield dev.write("UPD\r")
         ans = yield dev.read()
@@ -222,6 +251,11 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(209,channel='s',returns=['v','s'])
     def get_channel_voltage(self,c,channel):
+        """Queries the device for the voltage on a specific channel. Usage is get_channel_voltage(channel)
+Channel must be either "X1" "Y1" "X2" or "Y2\""""
+        if not (channel in ["X1","X2","Y1","Y2"]):
+                yield
+                returnValue("Error: invalid channel. It must be either \"X1\" \"Y1\" \"X2\" or \"Y2\"")
         dev=self.selectedDevice(c)
         yield dev.write("GET,%s\r"%channel)
         ans = yield dev.read()
@@ -233,6 +267,7 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(210,returns='v')
     def get_phase(self,c):
+        """Queries the device fot the phase offset. Usage is get_phase()"""
         dev=self.selectedDevice(c)
         yield dev.write("PHS?\r")
         ans=yield dev.read()
@@ -240,13 +275,16 @@ class AD5764AcboxServer(DeviceServer):
 
     @setting(211,returns='v')
     def get_frequency(self,c):
+        """Queries the device for the frequency setting. Usage is get_frequency()"""
         dev=self.selectedDevice(c)
         yield dev.write("FRQ?\r")
         ans = yield dev.read()
         returnValue(float(ans))
 
     @setting(8998)
-    def read_voltages(self,c):
+    def read_settings(self,c):
+        """Causes the server to query the device for its current settings. Usage is read_settings()
+It's only necessary to call this once on startup, as the server keeps track of the changes made."""
         dev=self.selectedDevice(c)
         for n in range(4):
             yield dev.write("GET,%s\r"%int_to_port[n])
@@ -261,8 +299,10 @@ class AD5764AcboxServer(DeviceServer):
         phs = yield dev.read()
         self.voltages[c['device']][6]=str(phs)
 
-    @setting(8999)
-    def get_voltages(self,c):
+    @setting(8999,returns='**s')
+    def get_settings(self,c):
+        """Returns the settings for all active devices. Usage is get_settings()
+Yields a list of form [[port,X1_voltage,Y1_voltage,X2_voltage,Y2_voltage,frequency,phase], ... ]"""
         ret = yield self.voltages
         returnValue(ret)
         
