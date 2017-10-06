d138a5d7ca45eb992371bb36a8ee97569103bd4c
ckometter <ckometter@gmail.com>
2017-10-06 18:07:15 -0500

    Update

--- beaglebone/spi_device_manager.py
+++ beaglebone/spi_device_manager.py
@@ -39,9 +39,77 @@ timeout = 5
 
 class SPIDeviceManager(LabradServer):
     """
-    TODO
+    Manages autodetection and identification of SPI devices.
+
+    The device manager listens for "SPI Device Connect" and
+    "SPI Device Disconnect" messages coming from SPI bus servers.
+    It attempts to identify the connected devices and forward the
+    messages on to servers interested in particular devices.
     """
     name = 'SPI Device Manager'
+
+    @inlineCallbacks
+    def initServer(self):
+        """Initialize the server after connecting to LabRAD."""
+        self.knownDevices = {} # maps (server, channel) to (name, idn)
+        self.deviceServers = {} # maps device name to list of interested servers.
+                                # each interested server is {'target':<>,'context':<>,'messageID':<>}
+        self.identLock = DeferredLock()
+
+        # named messages are sent with source ID first, which we ignore
+        connect_func = lambda c, (s, payload): self.spi_device_connect(*payload)
+        disconnect_func = lambda c, (s, payload): self.spi_device_disconnect(*payload)
+        mgr = self.client.manager
+        self._cxn.addListener(connect_func, source=mgr.ID, ID=10)
+        self._cxn.addListener(disconnect_func, source=mgr.ID, ID=11)
+        yield mgr.subscribe_to_named_message('SPI Device Connect', 10, True)
+        yield mgr.subscribe_to_named_message('SPI Device Disconnect', 11, True)
+
+        # do an initial scan of the available GPIB devices
+        yield self.refreshDeviceLists()
+
+    @inlineCallbacks
+    def refreshDeviceLists(self):
+        """Ask all SPI bus servers for their available SPI devices."""
+        servers = [s for n, s in self.client.servers.items()
+                     if (('SPI Bus' in n) or ('spi_bus' in n)) and \
+                        (('List Devices' in s.settings) or \
+                         ('list_devices' in s.settings))]
+        serverNames = [s.name for s in servers]
+        print 'Pinging servers:', serverNames
+        resp = yield DeferredList([s.list_devices() for s in servers])
+        for serverName, (success, addrs) in zip(serverNames, resp):
+            if not success:
+                print 'Failed to get device list for:', serverName
+            else:
+                print 'Server %s has devices: %s' % (serverName, addrs)
+                for addr in addrs:
+                    self.spi_device_connect(serverName, addr)
+
+    @inlineCallbacks
+    def spi_device_connect(self, spiBusServer, channel):
+        """Handle messages when devices connect."""
+        print 'Device Connect:', spiBusServer, channel
+        if (spiBusServer, channel) in self.knownDevices:
+            return
+        device, idnResult = yield self.lookupDeviceName(spiBusServer, channel)
+        if device == UNKNOWN:
+            device = yield self.identifyDevice(spiBusServer, channel, idnResult)
+        self.knownDevices[spiBusServer, channel] = (device, idnResult)
+        # forward message if someone cares about this device
+        if device in self.deviceServers:
+            self.notifyServers(device, spiBusServer, channel, True)
+
+    def spi_device_disconnect(self, server, channel):
+        """Handle messages when devices connect."""
+        print 'Device Disconnect:', server, channel
+        if (server, channel) not in self.knownDevices:
+            return
+        device, idnResult = self.knownDevices[server, channel]
+        del self.knownDevices[server, channel]
+        # forward message if someone cares about this device
+        if device in self.deviceServers:
+            self.notifyServers(device, server, channel, False)
 
 __server__ = SPIDeviceManager()
 
--- beaglebone/spi_server.py
+++ beaglebone/spi_server.py
@@ -14,11 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from labrad.server import LabradServer
+from labrad.server import LabradServer, setting
 from labrad import util
 from labrad.errors import DeviceNotSelectedError
 import labrad.units as units
 from twisted.internet.defer import inlineCallbacks
+from twisted.internet.reactor import callLater
+from bbio import *
+from bbio.platform.beaglebone.spi import SPIBus
 
 
 """
@@ -39,12 +42,116 @@ timeout = 20
 ### END NODE INFO
 """
 
+SPI = SPI1
+
+class Slot(object):
+    """
+    Slots
+    """
+    def __init__(self, address=None, type=None):
+        self.address = address
+        self.type = type
+        self.buscs = self.set_bus()
+        self.chipselects = self.set_chipselects()
+
+    def set_buscs(self):
+        if self.type == None:
+            return None
+        #TODO
+
+    def set_chipselects(self):
+        if self.address == None or self.type == None:
+            return dict()
+        #TODO
+
 
 class SPIBusServer(LabradServer):
-    """Provides direct access to GPIB-enabled devices."""
+    """Provides direct access to SPI-enabled devices."""
     name = '%LABRADNODE% SPI Bus'
+
+    def initServer(self):
+        # start refreshing only after we have started serving
+        # this ensures that we are added to the list of available
+        # servers before we start sending messages
+        self.devices = {}
+        callLater(0.1, self.startRefreshing)
+
+    def startRefreshing(self):
+        """Start periodically refreshing the list of devices.
+        The start call returns a deferred which we save for later.
+        When the refresh loop is shutdown, we will wait for this
+        deferred to fire to indicate that it has terminated.
+        """
+        self.refreshDevices()
+
+    def refreshDevices(self):
+        """
+        Refresh the list of known devices on this bus.
+        """
+        try:
+            slots = self.detectDevices()
+            addresses = [int(key) for key in slots]
+            additions = set(addresses) - set(self.devices.keys())
+            deletions = set(self.devices.keys()) - set(addresses)
+            for addr in additions:
+                try:
+                    dev = slots[addr]
+                    self.devices[addr] = dev
+                    self.sendDeviceMessage('SPI Device Connect', addr)
+                except Exception, e:
+                    print 'Failed to add ' + addr + ':' + str(e)
+            for addr in deletions:
+                del self.devices[addr]
+                self.sendDeviceMessage('SPI Device Disconnect', addr)
+        except Exception, e:
+            print 'Problem while refreshing devices: ', str(e)
+
+    def detectDevices(self):
+        """
+        Query each slot for connected devices and returns a dictionary {addresses -> Slots}.
+        """
+        #TODO
+
+    def sendDeviceMessage(self, msg, addr):
+        print msg + ': ' + addr
+        self.client.manager.send_named_message(msg, (self.name, addr))
+
+    def getDevice(self, c):
+        if 'addr' not in c:
+            raise DeviceNotSelectedError("No SPI address selected")
+        if c['addr'] not in self.devices:
+            raise Exception('Could not find device ' + c['addr'])
+        dev = self.devices[c['addr']]
+        return dev
+
+    @setting(0, addr='s', returns='s')
+    def address(self, c, addr=None):
+        """Get or set the GPIB address for this context.
+        To get the addresses of available devices,
+        use the list_devices function.
+        """
+        if addr is not None:
+            c['addr'] = addr
+        return c['addr']
+
+    @setting(1, data='*i', returns='*i')
+    def transfer(self, c, data):
+        dev = self.getDevice(c)
+        resp = SPI.transfer(dev.buscs, data)
+        return resp
+
+    @setting(20, returns='*s')
+    def list_devices(self, c):
+        """Get a list of devices on this bus."""
+        return sorted(self.devices.keys())
+
+    @setting(21)
+    def refresh_devices(self, c):
+        """Manually refresh devices"""
+        self.refreshDevices()
 
 __server__ = SPIBusServer()
 
 if __name__ == '__main__':
+    SPI.begin()
     util.runServer(__server__)
